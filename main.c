7d960a35bc073e7b9c4acc4b575a8cd8bb87566a
OlguD <olgudegirmenci34@gmail.com>
2024-08-06 13:21:10 +0300

    functions added

--- main.c
+++ main.c
@@ -1,21 +1,47 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
+struct Text{
+    char *text;
+};
 
-typedef struct {
-    char buffer[1000];
-} Text;
+int getTextSize(char *text);
+int getWordCount(char *text);
 
 
 int main() {
-    printf("Hello, World!\n");
+    struct Text text;
+    text.text = (char *)malloc(256 * sizeof (char));
+    if (text.text == NULL){
+        printf("Memory allocation failed.");
+        return 1;
+    }
+
+    printf("Enter a sentence: ");
+    fgets(text.text, 256, stdin);
+
+    int count = getWordCount(text.text);
+
+    printf("\nCount: %d\n", count);
     return 0;
 }
 
-int getTextSize(Text *text){
-
+int getTextSize(char *text){
+    return strlen(text);
 }
 
-int getWordCount(Text *text){
-
+int getWordCount(char *text){
+    int wordCount = 0;
+    int textSize = getTextSize(text);
+    for (int i=0; i<textSize; i++){
+        if (text[i] == '\0' && text[i + 1] == '\0'){
+            break;
+        } else{
+            if (text[i] != '\0'){
+                wordCount += 1;
+            }
+        }
+    }
+    return wordCount;
 }
