4ddf1c4ed8e0d8c53983d45d5c3e28ca08c0face
OlguD <olgudegirmenci34@gmail.com>
2024-08-06 12:55:27 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+
+typedef struct {
+    char buffer[1000];
+} Text;
+
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+int getTextSize(Text *text){
+
+}
+
+int getWordCount(Text *text){
+
+}
